bdd2ab870cc55bc65c491ffb04b4023d53ed7436
edas112 <you@example.com>
2024-04-28 14:00:54 +0300

    kortele

--- routes/footerSetter.routes.js
+++ routes/footerSetter.routes.js
@@ -4,4 +4,14 @@ const { postInfoFooter } = require('../controllers');
 
 const router = Router();
 
+router.post('/', async (req, res) => {
+  try {
+    const data = await postInfoFooter(req.body);
+    res.status(201).json(data);
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
