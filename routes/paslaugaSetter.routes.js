078d46324d462af03a9c5c755e210d692098d3bc
edas112 <you@example.com>
2024-04-28 14:00:08 +0300

    kortele

--- routes/paslaugaSetter.routes.js
+++ routes/paslaugaSetter.routes.js
@@ -14,4 +14,14 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.patch('/:id', async (req, res) => {
+  try {
+    const data = await postPaslauga(req.params.id, req.body);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
