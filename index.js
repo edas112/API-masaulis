10ff40d7108daaae94564753220c7135422c1782
edas112 <you@example.com>
2024-05-07 19:02:51 +0300

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRouter = require('./routes/user.routes');
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://192.168.1.124'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
+  // allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
   optionsSuccessStatus: 200,
 };
